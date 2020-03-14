ab5d8932cb732f48041707c5935ab7fac53ed0a6
Armadillo <ajknapp2015@gmail.com>
2020-03-14 15:04:13 -0700

    Renamed model to Workout and created api route to generate empty workout

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,19 +1,25 @@
+const Workout = require("../models/Workout");
 
 module.exports = function(app) {
 
-    app.get("/api/workouts", (req, res) => {
-        res.json("hello");
-    })
+    // app.get("/api/workouts", (req, res) => {
+    //     res.json("hello");
+    // })
 
     app.post("/api/workouts", (req, res) => {
-        res.json("hello");
+        Workout.create({})
+        .then((result) => {
+            res.json(result);
+        }).catch((err) => {
+            console.log(err);
+        })
     })
 
-    app.put("/api/workouts/:id", (req, res) => {
-        res.json("hello");
-    })
+    // app.put("/api/workouts/:id", (req, res) => {
+    //     res.json("hello");
+    // })
 
-    app.get("/api/workouts/range", (req, res) => {
-        res.json("hello");
-    })
+    // app.get("/api/workouts/range", (req, res) => {
+    //     res.json("hello");
+    // })
 }
